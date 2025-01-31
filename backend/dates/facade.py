4826febd3af07dace09baa467765e6a0643ee17b
Anton Curanz <anton@curanz.de>
2025-01-31 17:52:07 +0100

    maybe fix

--- backend/dates/facade.py
+++ backend/dates/facade.py
@@ -11,12 +11,12 @@ class DatesFacade:
         self.date_service = date_service
 
     def get_next_due_date(self, account: Account):
-        self.date_service.get_next_due_date(account)
+        return self.date_service.get_next_due_date(account)
 
     def get_last_due_date(self, account: Account):
-        self.date_service.get_last_due_date(account)
+        return self.date_service.get_last_due_date(account)
 
     def get_statement_date_for_due_date(self, account: Account, due_date):
-        self.date_service.get_statement_date_for_due_date(account, due_date)
+        return self.date_service.get_statement_date_for_due_date(account, due_date)
 
 DatesDep = Annotated[DatesFacade, Depends()]
