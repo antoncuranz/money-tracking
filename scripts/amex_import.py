179489bc129f56fb4e80f7bc60f80dfa630d5f9a
Anton Curanz <anton@curanz.de>
2025-04-29 22:02:06 +0200

    fix regex

--- scripts/amex_import.py
+++ scripts/amex_import.py
@@ -14,7 +14,7 @@ testmail_api_key = os.getenv("TESTMAIL_API_KEY")
 testmail_namespace = os.getenv("TESTMAIL_NAMESPACE")
 
 url = f"https://api.testmail.app/api/json?apikey={testmail_api_key}&namespace={testmail_namespace}"
-regex = r"<div style=\"[^\"]*\">\s*(\d\d)\.(\d\d)\.(\d\d\d\d)([^<]*)<\/div>.*<div style=\"[^\"]*\">\s*€(\d+(?:\.\d{3})?,\d\d)\s*<\/div>"
+regex = r"<\w+ style=\"[^\"]*\">\s*(\d\d)\.(\d\d)\.(\d\d\d\d)([^<]*)<\/\w+>.*<\w+ style=\"[^\"]*\">\s*€(\d+(?:\.\d{3})?,\d\d)\s*<\/\w+>"
 
 
 def import_transaction(transaction):
